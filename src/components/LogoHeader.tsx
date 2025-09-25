619bd55831456a71440655175ff3a5e6f1d70b92
CaioCesarMDS <caiocesarsts@gmail.com>
2025-09-25 09:01:54 -0300

    chore: update project name, add package-lock.json in gitignore

--- src/components/LogoHeader.tsx
+++ src/components/LogoHeader.tsx
@@ -5,7 +5,7 @@ export default function LogoHeader() {
     <div className="flex flex-col items-center pb-24">
       <div className="mb-2 flex items-center space-x-2">
         <img src={logo} alt="Logo da Defesa Civil" className="w-16" />
-        <h1 className="text-3xl font-bold text-orange-500">PernambuBox</h1>
+        <h1 className="text-3xl font-bold text-orange-500">PernamBox</h1>
       </div>
       <p className="text-sm text-gray-400 italic">
         Controle de Estoque Integrado
