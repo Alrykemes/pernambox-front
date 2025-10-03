fd48c687f6c1b2da26dc4f7aa18ccc8edf0bb205
CaioCesarMDS <caiocesarsts@gmail.com>
2025-10-02 23:36:10 -0300

    feat: add AppButton component and refactor LogoHeader to use Link for navigation; remove SubmitButton component

--- src/components/LogoHeader.tsx
+++ src/components/LogoHeader.tsx
@@ -1,12 +1,13 @@
 import logo from "@/assets/images/logo.png";
+import { Link } from "react-router-dom";
 
 export default function LogoHeader() {
   return (
     <div className="flex flex-col items-center pb-24">
-      <div className="mb-2 flex items-center space-x-2">
+      <Link to="/" className="mb-2 flex items-center space-x-2">
         <img src={logo} alt="Logo da Defesa Civil" className="w-16" />
         <h1 className="text-3xl font-bold text-orange-500">PernamBox</h1>
-      </div>
+      </Link>
       <p className="text-sm text-gray-400 italic">
         Controle de Estoque Integrado
       </p>
